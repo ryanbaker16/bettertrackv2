0efba13531084e4570532f9d88ece03fb53f60cf
Ryan Baker <rjbx@umich.edu>
2021-11-28 19:27:52 -0500

    login broke

--- script.js
+++ script.js
@@ -11,6 +11,12 @@ var resultView = new Vue({
       feels_entry: '',
       goals_entry: '',
       misc_entry: '',
+      total_stars: [0,0,0,0,0,0,0,0,0,0,0,0],
+      num_checkins: [3,3,3,3,3,3,3,3,3,3,3,3],
+      months: ["December 2021", "November 2021", "October 2021", "September 2021", "August 2021","July 2021", "June 2021", "May 2021", "April 2021", "March 2021", "February 2021", "January 2021"],
+      indexArray: [0,1,2,3,4,5,6,7,8,9,10,11],
+      avgStars: [0,0,0,0,0,0,0,0,0,0,0,0]
+
 
     },
     methods: {
@@ -26,7 +32,8 @@ var resultView = new Vue({
                     console.log("successfully logged in");
                     console.log(this.currentUser);
                     document.getElementById("login").style.visibility = "hidden";
-                    document.getElementById("prizes").style.visibility = "visible";
+                    document.getElementById("checkin").style.visibility = "visible";
+                    document.getElementById("navbar").style.visibility = "visible";
                 }
                 else{
                     alert("Invalid Password: Please try again");
@@ -52,8 +59,24 @@ var resultView = new Vue({
             //redirect to login page
         },
 
-        openPrizesDiv(){
+        showPrizes(){
+            document.getElementById('prizes').style.visibility = "visible";
+            document.getElementById('checkin').style.visibility = "hidden";
+            document.getElementById('diary').style.visibility = "hidden";
+            
+        },
 
+        showCheckin(){
+            document.getElementById('prizes').style.visibility = "hidden";
+            document.getElementById('checkin').style.visibility = "visible";
+            document.getElementById('diary').style.visibility = "hidden";
+            
+        },
+
+        showDiary(){
+            document.getElementById('prizes').style.visibility = "hidden";
+            document.getElementById('checkin').style.visibility = "hidden";
+            document.getElementById('diary').style.visibility = "visible";
         },
 
         getUserStreak(){
@@ -74,11 +97,18 @@ var resultView = new Vue({
           console.log(checkinLog);
         },
 
-
-
-
-
-
+        calcAverage: function() {
+                  for (let i = 0; i < this.currentUser['checkins'].length; i++) {
+                        var total = 0;
+                        var date = Date.parse(this.currentUser['checkins'][i].checkin_date)
+                        var month = (date.getMonth() + 1);
+                        console.log(month)
+                        totalStars[month] += this.currentUser['checkins'][i].checkin_rating;
+                    }
+                  for (let i = 0; i < 12; i++) {
+                      avgStars[i] += totalStars[i]/num_checkins[i];
+                  }
+        }
     }
 
   })
