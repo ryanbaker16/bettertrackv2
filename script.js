e311e2b5a0caa79764e2c34265f05cfadeba11f1
paarth-s <47118966+paarth-s@users.noreply.github.com>
2021-11-29 14:12:45 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -16,20 +16,7 @@ var resultView = new Vue({
     totalStars: [0,0,0,0,0,0,0,0,0,0,0,0],
     avgStars: [0,0,0,0,0,0,0,0,0,0,0,0],
     indexArray: [0,1,2,3,4,5,6,7,8,9,10,11],
-    checks1: [],
-    checks2: [],
-    checks3: [],
-    checks4: [],
-    checks5: [],
-    checks6: [],
-    checks7: [],
-    checks8: [],
-    checks9: [],
-    checks10: [],
-    checks11: [],
-    checks12: [],
-    months: ["January 2021", "February 2021", "March 2021", "April 2021", "May 2021", "June 2021", "July 2021", "August 2021", "September 2021", "October 2021", "November 2021", "December 2021"]
-
+    months: ["December 2021", "November 2021", "October 2021", "September 2021", "August 2021", "July 2021", "June 2021", "May 2021", "April 2021", "March 2021", "February 2021", "January 2021"]
 
     },
     methods: {
@@ -110,6 +97,10 @@ var resultView = new Vue({
             'checkin_date': dateString,
             'month': dateMonth
           }
+          this.currentUser['checkins'].push(checkinLog);
+          this.currentUser['num_checkins']++;
+          this.currentUser['checkin_streak']++;
+          this.getUserStreak();
           console.log(checkinLog);
         },
 
@@ -123,7 +114,21 @@ var resultView = new Vue({
                   for (let i = 0; i < 12; i++) {
                       avgStars[i] += totalStars[i]/num_checkins[i];
                   }
+        },
+
+        calculatePrizeWidth(){
+
+                if(this.userStreak < 5){
+                    document.getElementById('prizebar').style.width = (this.userStreak*100/5).toString()+"%";
+                }
+                else if(this.userStreak < 10){
+                    document.getElementById('prizebar').style.width = (this.userStreak*100/10).toString()+"%";
+                }
+                else if (this.userStreak < 25){
+                    document.getElementById('prizebar').style.width = (this.userStreak*100/30).toString()+"%";
+                }
         }
     }
+
 
   })
