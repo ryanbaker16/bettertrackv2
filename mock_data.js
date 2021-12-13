ea50d51fce2c7f37bd90b17e368f762344287799
Ryan Baker <rjbx@umich.edu>
2021-12-12 22:44:33 -0500

    made changes for demo

--- mock_data.js
+++ mock_data.js
@@ -3,12 +3,12 @@ var users = {
         "ryan": {
             "password": "password",
             "name": "Johnny Appleseed",
-            "num_checkins" : 21,
-            "checkin_streak": 8,
+            "num_checkins" : 3,
+            "checkin_streak": 0,
             "checkins": [
-                {"checkin_rating": 9, "feels": "good", "goals": "pass my exam", "misc": "parallel parked successfully!", "checkin_date": "Nov 28 2021", "month": 11},
-                {"checkin_rating": 3, "feels": "bad", "goals": "clean my room", "misc": "failed my exam", "checkin_date": "Nov 28 2021", "month": 11},
-                {"checkin_rating": 5, "feels": "mid", "goals": "cook dinner", "misc": "went to the grocery", "checkin_date": "Nov 28 2021", "month": 11}
+                {"checkin_rating": 9, "feels": "Really awesome!", "goals": "pass my exam", "misc": "parallel parked successfully!", "checkin_date": "Nov 28 2021", "month": 11},
+                {"checkin_rating": 4, "feels": "I'm alright, stressed.", "goals": "clean my room", "misc": "failed my exam", "checkin_date": "Nov 27 2021", "month": 11},
+                {"checkin_rating": 5, "feels": "Pretty average day.", "goals": "cook dinner", "misc": "went to the grocery", "checkin_date": "Nov 26 2021", "month": 11}
             ]
         },
         "paarth": {
